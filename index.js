fd0780e870d50e69d8e54556ca8e499af66eaa09
NMVikings <no.more.vikings@gmail.com>
2017-11-03 13:17:41 +0300

    Fix counter and add preventDefault

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const dinamo = document.getElementById("dinamo")
+const result = document.getElementById('result')
 let counter = 0
 
-dinamo.addEventListener("touchmove", () => {
+dinamo.addEventListener("touchmove", (e) => {
+	e.preventDefault()
 	counter++
+	result.textContent = counter
 	if (counter === 100) {
 		alert(100)
 	}
+
 })
