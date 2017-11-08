6f05e9c8feca58d0d990980eaa97c5d14fe3bbe8
NMVikings <no.more.vikings@gmail.com>
2017-11-08 15:26:57 +0300

    Add timestamps

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const rotate = document.getElementById("rotate")
 const result = document.getElementById('result')
 let counter = 0
 let subCounter = new Set()
-let previousAngle = 0;
+let previousTimestamp = Date.now();
 
-isNear = (num, anchor) => anchor - num < 5 && anchor - num > -5
+isNear = (num, anchor) => anchor - num <= 9 && anchor - num >= -9
+
+addCounter = () => {
+	const currentTime = Date.now()
+	const newValue = Math.ceil(Math.random() * 3 * Math.max(1, 100 / (currentTime - previousTimestamp)))
+	console.log(newValue)
+	counter += newValue
+	previousTimestamp = currentTime
+}
 
 rotate.addEventListener("touchmove", (e) => {
 	e.preventDefault()
@@ -18,22 +26,49 @@ rotate.addEventListener("touchmove", (e) => {
 
 	rotate.style.transform = `rotate(${angle / 2}deg)`;
 
-	if (isNear(angle, 0)) {
+	if (isNear(angle, 0) && !subCounter.has(0)) {
+		addCounter()
 		subCounter.add(0)
 	}
-	if (isNear(angle, 90)) {
-		subCounter.add(90)
+	if (isNear(angle, 36) && !subCounter.has(36)) {
+		addCounter()
+		subCounter.add(36)
 	}
-	if (isNear(angle, -90)) {
-		subCounter.add(-90)
+	if (isNear(angle, 72) && !subCounter.has(72)) {
+		addCounter()
+		subCounter.add(72)
 	}
-	if (isNear(angle, 180) || isNear(angle, -180)) {
+	if (isNear(angle, 108) && !subCounter.has(108)) {
+		addCounter()
+		subCounter.add(108)
+	}
+	if (isNear(angle, 144) && !subCounter.has(144)) {
+		addCounter()
+		subCounter.add(144)
+	}
+	if (isNear(angle, -36) && !subCounter.has(-36)) {
+		addCounter()
+		subCounter.add(-36)
+	}
+	if (isNear(angle, -72) && !subCounter.has(-72)) {
+		addCounter()
+		subCounter.add(-72)
+	}
+	if (isNear(angle, -108) && !subCounter.has(-108)) {
+		addCounter()
+		subCounter.add(-108)
+	}
+	if (isNear(angle, -144) && !subCounter.has(-144)) {
+		addCounter()
+		subCounter.add(-144)
+	}
+	if (isNear(angle, 180) || isNear(angle, -180) && !subCounter.has(180)) {
+		addCounter()
 		subCounter.add(180)
 	}
 
-	if (subCounter.size === 4) {
+	if (subCounter.size === 10) {
 		subCounter.clear()
-		counter += 10
 	}
 	previousAngle = angle;
 })
