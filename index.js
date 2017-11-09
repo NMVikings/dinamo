6c55693c140a3db0c16f834b1768024ccbba0177
NMVikings <no.more.vikings@gmail.com>
2017-11-09 13:36:42 +0300

    Refactor code

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
 const dinamo = document.getElementById("dinamo")
 const rotate = document.getElementById("rotate")
 const result = document.getElementById('result')
+const box = rotate.getBoundingClientRect()
+const breakpoints = [0, 72, 144, -144, -72]
+const interval = 360 / (breakpoints.length * 4)
+const breakpointsCounter = new Set()
+const boxCenter=[ box.x + box.width/2, box.y + box.height/2 ]
+
 let counter = 0
-let subCounter = new Set()
-let previousTimestamp = Date.now();
+let timer = 0
+let previousTimestamp = 0
 
-isNear = (num, anchor) => anchor - num <= 18 && anchor - num >= -18
+const isNear = (num, anchor) => anchor - num <= interval && anchor - num >= -interval
 
-addCounter = () => {
-	const currentTime = Date.now()
-	const newValue = Math.ceil(Math.random() * 6 * Math.max(1, 100 / (currentTime - previousTimestamp)))
-	console.log(newValue)
-	counter += newValue
-	previousTimestamp = currentTime
+const changeCounterValue = (value) => {
+	counter = Math.min(Math.max(counter + value, 0), 10000)
+	result.textContent = counter
+
+	if (counter === 10000) {
+		clearInterval(timer)
+	}
 }
+
+const increaseCounterValue = () => {
+	const currentTime = Date.now()
+	const newValue = Math.ceil(Math.random() * 6) * Math.floor(Math.max(1, 100 / (currentTime - previousTimestamp)))
+	previousTimestamp = currentTime
+
+	changeCounterValue(newValue)
+}
+
+
+const checkBreakpoint = (angle) =>{
+	for (let breakpoint of breakpoints) {
+		if (isNear(angle, breakpoint) && !breakpointsCounter.has(breakpoint)) {
+			increaseCounterValue()
+			breakpointsCounter.add(breakpoint)
+		}
+	}
+
+	if (breakpointsCounter.size === breakpoints.length) {
+		breakpointsCounter.clear()
+	}
+}
+
+const changeAngle = (angle) => {
+	rotate.style.transform = `rotate(${angle}deg)`
+}
+
+const startTick = () => setInterval(() => {
+	if (Date.now() - previousTimestamp > 600) {
+		changeCounterValue(-1)
+	}
+}, 100)
+
+startTimer = () => {
+	if (previousTimestamp === 0) {
+		timer = startTick()
+	}
+}
+
+const calculateAngle = (e) =>
+	(Math.atan2(e.targetTouches[0].pageX - boxCenter[0], - (e.targetTouches[0].pageY - boxCenter[1]) )*(180/Math.PI)).toFixed(0)
 
 rotate.addEventListener("touchmove", (e) => {
 	e.preventDefault()
-	// counter++
-	result.textContent = counter
-	const box = rotate.getBoundingClientRect()
-	var boxCenter=[box.x + box.width/2, box.y + box.height/2];
 
-	var angle = (Math.atan2(e.targetTouches[0].pageX - boxCenter[0], - (e.targetTouches[0].pageY - boxCenter[1]) )*(180/Math.PI)).toFixed(0);
-
-	rotate.style.transform = `rotate(${angle / 2}deg)`;
-
-	if (isNear(angle, 0) && !subCounter.has(0)) {
-		addCounter()
-		subCounter.add(0)
-	}
-	if (isNear(angle, 72) && !subCounter.has(72)) {
-		addCounter()
-		subCounter.add(72)
-	}
-	if (isNear(angle, 144) && !subCounter.has(144)) {
-		addCounter()
-		subCounter.add(144)
-	}
-	if (isNear(angle, -72) && !subCounter.has(-72)) {
-		addCounter()
-		subCounter.add(-72)
-	}
-	if (isNear(angle, -144) && !subCounter.has(-144)) {
-		addCounter()
-		subCounter.add(-144)
-	}
-
-	if (subCounter.size === 5) {
-		subCounter.clear()
-	}
-	previousAngle = angle;
+	startTimer()
+	const angle = calculateAngle(e)
+	checkBreakpoint(angle)
+	changeAngle(angle)
 })
